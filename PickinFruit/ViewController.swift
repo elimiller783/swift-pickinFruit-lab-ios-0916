baeb2851981c9a3775e0c9a488b2b8333016f37a
elimiller783 <emiller783@gmail.com>
2016-10-20 21:18:17 -0400

    done

--- PickinFruit/ViewController.swift
+++ PickinFruit/ViewController.swift
@@ -8,27 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    
+    
+    @IBOutlet weak var fruitPicker: UIPickerView!
     @IBOutlet weak var spinButton: UIButton!
     @IBOutlet weak var resultLabel: UILabel!
     
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return fruitsArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+       return fruitsArray.count
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
     var fruitsArray = ["🍎", "🍊", "🍌", "🍐", "🍇", "🍉", "🍓", "🍒", "🍍"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.spinButton.accessibilityLabel = Constants.SPIN_BUTTON
+        self.fruitPicker.delegate = self
+        self.fruitPicker.dataSource = self
         
     }
     
     @IBAction func spinButtonTapped(_ sender: UIButton) {
-        
-        
-        
+        fruitPicker.selectRow(Int(arc4random_uniform(UInt32(fruitsArray.count))), inComponent: 0, animated: true)
+        fruitPicker.selectRow(Int(arc4random_uniform(UInt32(fruitsArray.count))), inComponent: 1, animated: true)
+        fruitPicker.selectRow(Int(arc4random_uniform(UInt32(fruitsArray.count))), inComponent: 2, animated: true)
+    
+        if fruitPicker.selectedRow(inComponent: 0) == fruitPicker.selectedRow(inComponent: 1) &&
+            fruitPicker.selectedRow(inComponent: 1) == fruitPicker.selectedRow(inComponent: 2) {
+            resultLabel.text = "WINNER!"
+        } else {
+            resultLabel.text = "TRY AGAIN"
+        }
+        }
     }
+    
+    
 
-}
+    
+    
+
 
 // MARK: Set Up
 extension ViewController {
